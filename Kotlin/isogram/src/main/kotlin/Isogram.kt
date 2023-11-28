9c2318721ef9784e195d39964ec225040f298fb8
AnuradhaMandapuram <anuradha.mandapuram@gmail.com>
2023-11-28 23:26:30 +0530

    adding first program

---
+++ Kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -0,0 +1,8 @@
+object Isogram {
+
+    fun isIsogram(input: String): Boolean {
+        for(letter in 'a'..'z')
+     if(input.toLowerCase().count { it == letter } > 1 ) return false
+	 return true
+    }
+}
